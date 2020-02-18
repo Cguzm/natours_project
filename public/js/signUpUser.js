a08767caf1808836fc855a92edca2342da4850b7
ricky <riquelviconteras@hotmail.com>
2020-02-18 00:59:30 -0500

    Added signup form

---
+++ public/js/signUpUser.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const signUpUser = async data => {
+  try {
+    const url = 'http://127.0.0.1:3000/api/v1/users/signup';
+    const res = await axios({
+      method: 'POST',
+      url,
+      data
+    });
+    if (res.data.status === 'success') {
+      showAlert('success', `User created successfully`);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
